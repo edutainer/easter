ca738f5cf49d99bb7ad405f14f8be666558d0d93
edutainer <l.m.kollar@gmail.com>
2020-02-03 21:01:15 +0100

    Lepesenkent kulon directory

---
+++ Easter3/scripts.js
@@ -0,0 +1,1 @@
+new Game('game');// játék indítása
---
+++ Easter7/src/Egg.js
@@ -0,0 +1,15 @@
+class egg extends RigidBody{
+    constructor(parElObj, elemClass) {
+        super(parElObj, elemClass); // A szülő konstruktora
+        this.configMove();  // a mozgás jellemzőinek meghatározás
+        this.Move();        // a mozgás kezelésének indítása
+    }
+    configMove(){
+        this.movingStep = () => this.y = this.y - 2; // elmozdulás lefele minden lépésben
+        // pozicionálás
+        this.y = this.maxY(); // lehető legmagasabbra
+        this.x = Math.round(Math.random() * this.maxX());//Random x, de szülőelemre ráférjen.
+        // Beavatkozást igénylő helyzetek
+        //this.isPotentialCollision = () => this.y <= this.parElementObj.hat.height;
+    }
+}
